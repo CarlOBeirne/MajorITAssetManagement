9e2048bc77e95570e91b260519093c63412ea766
Carl <carl.obeirne@gmail.com>
2018-10-30 20:56:15 +0000

    Trying to revert and add login page

---
+++ WebApplication1/WebApplication1/login.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace WebApplication1
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Login_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MITAMconnectionString"].ConnectionString);
+            conn.Open();
+            string checkuser = "select count(*) from Users where Email='" + Email.Text + "'";
+            SqlCommand com = new SqlCommand(checkuser, conn);
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            if (temp == 1)
+            {
+                //conn.Open();
+                string checkPasswordQuery = "select PasswordHash from Users where Email='" + Email.Text + "'";
+                SqlCommand passComm = new SqlCommand(checkPasswordQuery, conn);
+                string password = passComm.ExecuteScalar().ToString().Replace(" ","");
+                if(password == PasswordHash.Text)
+                {
+                    Session["New"] = Email.Text;
+                    Response.Write("Password is correct");
+                    Response.Redirect("users.aspx");
+                }
+                else
+                {
+                    Response.Write("Password is not correct");
+                }
+            }
+            else
+            {
+                Response.Write("Username is not correct");
+            }
+            conn.Close();
+
+        }
+    }
+}
